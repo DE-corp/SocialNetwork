f07c6fc94cfb7ad17094d48ccd066e794964b812
DE-corp <crazyHOPHOP@rambler.ru>
2023-05-08 17:35:17 +0300

    Добавьте файлы проекта.

---
+++ SocialNetwork.PLL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocialNetwork.PLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
