9398fba45667f4626fd429a9cf08f8ac0490dc18
DE-corp <crazyHOPHOP@rambler.ru>
2023-05-12 16:14:01 +0300

    Написал тесты SocialNetwork.BLL

---
+++ SocialNetwork.BLL.Tests/FriendServiceTests.cs
@@ -0,0 +1,27 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+using Xunit;
+
+namespace SocialNetwork.BLL.Tests
+{
+    public class FriendServiceTests
+    {
+        [Fact]
+        public void AddFriendMustThrowException()
+        {
+            var addFriendData = new AddFriendData()
+            {
+                FriendEmail = "dfffff",
+                UserId = 7
+            };
+
+            var friendService = new FriendService();
+
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                friendService.AddFriend(addFriendData);
+            });
+        }
+    }
+}
---
+++ SocialNetwork.BLL.Tests/UserServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using SocialNetwork.BLL.Models;
+using SocialNetwork.BLL.Services;
+using System;
+using Xunit;
+
+namespace SocialNetwork.BLL.Tests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void RegisterMustThrowException()
+        {
+            UserRegistrationData userRegistrationData = new UserRegistrationData()
+            {
+                Email = "sssdasd",
+                FirstName = "",
+                LastName = "Иванов",
+                Password = "1234567"
+            };
+
+            var userService = new UserService();
+
+            Assert.Throws<ArgumentNullException>(delegate
+            {
+                userService.Register(userRegistrationData);
+            });
+        }
+    }
+}
